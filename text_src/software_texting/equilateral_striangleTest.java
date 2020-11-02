4584c6fdb3a93464b4d7e093eb661936a08b9475
APTX486913 <1264012866@qq.com>
2020-11-02 11:23:43 +0800

    添加注释

--- text_src/software_texting/equilateral_striangleTest.java
+++ text_src/software_texting/equilateral_striangleTest.java
@@ -3,11 +3,11 @@ package software_texting;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+//等边三角形测试类
 public class equilateral_striangleTest {
 
 
-
+	//是否为等边三角形测试类
 	@Test
 	public void testChecktype_equilateral_striangle() {
 		equilateral_striangle t1=new equilateral_striangle(4,4,4);
@@ -16,7 +16,7 @@ public class equilateral_striangleTest {
 		boolean expected=true;
 		assertEquals(expected,actual);
 	}
-
+	//返回文字结果测试类
 	@Test
 	public void testGetMessage() {
 		equilateral_striangle t1=new equilateral_striangle(4,4,4);
@@ -25,5 +25,4 @@ public class equilateral_striangleTest {
 		String expected="边长属于等边三角形";
 		assertEquals(expected,actual);
 	}
-
 }
